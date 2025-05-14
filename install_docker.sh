e93dc1eb1255e88ea51193f48c92af82b0f2a77e
Veera Prakash <veeraprakash.bg@gmail.com>
2025-05-14 07:56:51 +0530

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -3,7 +3,6 @@
 #check whether root user or not
 R="\e[31m"
 N="\e[0m"
-
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
